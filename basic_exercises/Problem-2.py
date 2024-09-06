71cf26137d5eb8e52b6182dc341041e39a9651cf
Ahmad Nani <phantom.an95g@gmail.com>
2024-09-06 10:15:44 +0300

    Write A Python Program To Take Mark From The User To Check Whether User Able To Admission In College Or Not

--- basic_exercises/Problem-2.py
+++ basic_exercises/Problem-2.py
@@ -5,10 +5,12 @@
 # Algorithm:
 # 01: Ask the user to insert his mark
 # 02: Use if statement to print if the user is able to addmission in collage or not
+try:
+    mark = int(input("Please Enter Your Mark: ").strip()) # Take the user mark
 
-mark = int(input("Please Enter Your Mark: ")) # Take the user mark
-
-if mark < 60 : # Check if user is accepted or not and print message.
-    print(f"Sorry! Your mark does not allow you to join our college, You needed {60 - mark} more points to be accepted")
-else:
-    print("Congrats, Your mark allows you to join our college")
+    if mark < 60 : # Check if user is accepted or not and print message.
+        print(f"Sorry! Your mark does not allow you to join our college, You needed {60 - mark} more points to be accepted")
+    else:
+        print("Congrats, Your mark allows you to join our college")
+except ValueError:
+    print("Wrong input! Please Enter Only Your Mark")
